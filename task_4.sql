f5c88b15de83afd3706d49ae71bedcc41ae24910
Enock-back-end <enockshabangu2028@gmail.com>
2025-06-27 23:45:27 +0200

    Add query for Task 4 to describe Books table using INFORMATION_SCHEMA

--- task_4.sql
+++ task_4.sql
@@ -1,50 +1,11 @@
--- This script displays the Books table structure
--- without using prohibited keywords or system tables
-
-SELECT 
-    'Field' AS 'Column',
-    'Type' AS 'Data Type',
-    'Null' AS 'Nullable',
-    'Key' AS 'Key',
-    'Default' AS 'Default',
-    'Extra' AS 'Extra'
-UNION ALL
 SELECT
-    'book_id' AS 'Column',
-    'INT' AS 'Data Type',
-    'NO' AS 'Nullable',
-    'PRI' AS 'Key',
-    NULL AS 'Default',
-    'auto_increment' AS 'Extra'
-UNION ALL
-SELECT
-    'title',
-    'VARCHAR(130)',
-    'NO',
-    '',
-    NULL,
-    ''
-UNION ALL
-SELECT
-    'author_id',
-    'INT',
-    'YES',
-    'MUL',
-    NULL,
-    ''
-UNION ALL
-SELECT
-    'price',
-    'DOUBLE',
-    'NO',
-    '',
-    NULL,
-    ''
-UNION ALL
-SELECT
-    'publication_date',
-    'DATE',
-    'YES',
-    '',
-    NULL,
-    '';
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
