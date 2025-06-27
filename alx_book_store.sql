7a5c9e30582d860000943fc5850bbbc608318a74
Enock-back-end <enockshabangu2028@gmail.com>
2025-06-27 18:24:13 +0200

    Applied final corrections to alx_book_store.sql for checker

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,5 @@
--- Create the database if it does not exist
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
--- Use the alx_book_store database
 USE alx_book_store;
 
 CREATE TABLE IF NOT EXISTS Authors (
