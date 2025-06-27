52f7b324602b3220fca8ca262fe76dcdc06838c9
Enock-back-end <enockshabangu2028@gmail.com>
2025-06-27 21:19:10 +0200

    Fix: corrected foreign key casing and structure

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS Customers (
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
-    order_date DATE NOT NULL,
+    order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
 );
 
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity DOUBLE NOT NULL,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders (order_id),
     FOREIGN KEY (book_id) REFERENCES Books (book_id)
 );
