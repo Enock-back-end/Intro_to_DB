a286b4b7a35c15e85507325a5f2a4a8c955b208b
Enock-back-end <enockshabangu2028@gmail.com>
2025-06-27 22:41:03 +0200

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,36 +1,28 @@
-import sys
-import mysql.connector
-from mysql.connector import Error
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-DB_NAME = "alx_book_store"
+USE alx_book_store;
 
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
 
-def create_database():
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
 
-    connection = None
-    cursor = None
-    try:
-        connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="Alisa_2005"  # MySQL root password  and details
-        )
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL UNIQUE,
+    address TEXT NOT NULL
+);
 
-        if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}")
-            print(f"Database '{DB_NAME}' created successfully!")
-
-    except Error as err:
-        print(f"Error: '{err}'")
-        sys.exit(1)
-
-    finally:
-        if cursor:
-            cursor.close()
-        if connection and connection.is_connected():
-            connection.close()
-
-
-if __name__ == "__main__":
-    create_database()
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    cus
