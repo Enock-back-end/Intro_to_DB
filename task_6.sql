f8070f976c8d960c5a9317ec9bb434fb398d873a
Enock-back-end <enockshabangu2028@gmail.com>
2025-06-28 04:36:23 +0200

    FIX: Final correct format for task 6

--- task_6.sql
+++ task_6.sql
@@ -1,13 +1,6 @@
-INSERT INTO
-    customer (
-        customer_id,
-        customer_name,
-        email,
-        address
-    )
-VALUES (
-        1,
-        'Cole Baidoo',
-        'cbaidoo@sandtech.com',
-        '123 Happiness Ave.'
-    );
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(2, 'Blessing Malik', 'eobed@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '124 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
